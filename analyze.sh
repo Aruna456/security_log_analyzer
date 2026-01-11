4b2720224fd87d3b33957d600bd383fca653de1f
Aruna <arunasubramanian456@gmail.com>
2026-01-11 18:41:55 +0000

    Added: CLI Support

--- analyze.sh
+++ analyze.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-LOG_FILE="sample.log"
+#check if argument is provided
+if [ "$#" -ne 1 ]; then
+ echo "Usage: $0 <log_file>"
+ exit 2
+fi
+
+LOG_FILE="$1"
 REPORT_FILE="report.txt"
+
+#check if file exists
+if [ ! -f "$LOG_FILE" ]; then
+ echo "Error: File '$LOG_FILE' not found"
+ exit 3
+fi
 
 TOTAL_LINE=$(wc -l < "$LOG_FILE")
 ERROR_COUNT=$(grep -c "ERROR" "$LOG_FILE")
